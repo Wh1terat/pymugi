f28ee50dab0dbdb68d458a50856e1e5ebfeef5e8
Gareth Bryan <gaz@mx9.org>
2021-03-05 17:05:58 +0000

    Bump to v0.1.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="pymugi",
-    version="0.1.1",
+    version="0.1.2",
     packages=["pymugi"],
     author="Gareth Bryan",
     author_email="gareth@mx9.org",
